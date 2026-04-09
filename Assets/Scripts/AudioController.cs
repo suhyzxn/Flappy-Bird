e554f3b6ab9dd2af9dc6ca41b8fe64d9ba53273c
suhyzxn <kimsuhyeon360@gmail.com>
2026-04-09 16:51:35 +0900

    fix: Not synchronized with slider when restarting

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -7,31 +7,38 @@ public class AudioController : MonoBehaviour
     [SerializeField] AudioMixer audioMixer;
     [SerializeField] Slider SFXSlider;
     [SerializeField] Slider BGMSlider;
+    [SerializeField] AudioSource bgm;
     
     void Awake()
     {
-        if (PlayerPrefs.HasKey("SFX"))
-        {
-            SFXSlider.value = PlayerPrefs.GetFloat("SFX");
-        }
-        
-        if (PlayerPrefs.HasKey("BGM"))
-        {
-            BGMSlider.value = PlayerPrefs.GetFloat("BGM");
-        }
+        SFXSlider.onValueChanged.RemoveAllListeners();
+        BGMSlider.onValueChanged.RemoveAllListeners();
+    }
+
+    void Start()
+    {
+        SFXSlider.value = PlayerPrefs.GetFloat("SFX", 0.7f);
+        SetSFX(SFXSlider.value);
+    
+        BGMSlider.value = PlayerPrefs.GetFloat("BGM", 0.7f);
+        SetBGM(BGMSlider.value);
         
         SFXSlider.onValueChanged.AddListener(SetSFX);
         BGMSlider.onValueChanged.AddListener(SetBGM);
+        
+        bgm.Play();
     }
 
     void SetSFX(float volume)
     {
+        Debug.Log("SetSFX called: " + volume);
         audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("SFX", volume);
     }
 
     void SetBGM(float volume)
     {
+        Debug.Log("SetBGM called: " + volume);
         audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("BGM", volume);
     }
